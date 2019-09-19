bb63c4e822bd285987a60b00317341345408d2d7
Kevin Rivera <kevinrivera.a4b@gmail.com>
2019-09-19 11:07:08 +0200

    changues

---
+++ src/components/marketing/marketingPage.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class MarketingPage extends Component {
+    render(){
+        return(
+            <section>
+                <div></div>
+                <div></div>
+            </section>
+        )
+    }
+}
+
+export default MarketingPage
