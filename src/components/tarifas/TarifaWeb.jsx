1cab556742bd80f03234ad3f4e250ed3740102a2
Kevin <kevinrivera.a4b@gmail.com>
2019-09-23 17:18:01 +0200

    auto tarifas

--- src/components/tarifas/TarifaWeb.jsx
+++ src/components/tarifas/TarifaWeb.jsx
@@ -3,13 +3,19 @@ import React , {Component} from 'react'
 class Card extends Component{
     render(){
         return(
+            /*
+                titulo pack
+                descripcion pack
+                precio pack
+                caracteristicas = []
+            */ 
             <div className="w-25 bg-card mgw2 pad-full-1 radius shadow jus-center">
                 <div>
-                    <h3 className="tx-bold tx-center tx-blanco">Pack Basic</h3>
-                    <p className="tx-blanco tx-center">Para pymes que buscan crecer</p>
+                    <h3 className="tx-bold tx-center tx-blanco">{this.props.nombre}</h3>
+                    <p className="tx-blanco tx-center">{this.props.descripcion}</p>
                 </div>
                 <div>
-                    <h2 className="super-title tx-center tx-bold">$399</h2>
+                    <h2 className="super-title tx-center tx-bold">{this.props.precio}</h2>
                 </div>
                 <div>
                     <ul className="no-list">
@@ -28,12 +34,49 @@ class Card extends Component{
     }
 }
 class TarifaWeb extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            tarifas:[
+                {
+                    name:'Pack Pymes',
+                    descripcion:'Para pymes que buscan crecer',
+                    precio:'399$',
+                    caracteristicas:[
+                        "Autoadministrable",
+                        "Wordpress",
+                        "Seo"
+                    ]
+                },
+                {
+                    name: 'Pack Pro',
+                    descripcion: 'Para empresas consolidadas que buscan expandirse',
+                    precio: '699$',
+                    caracteristicas: [
+                        "Autoadministrable",
+                        "Wordpress",
+                        "Seo"
+                    ]
+                },
+                {
+                    name: 'Pack Enterprise',
+                    descripcion: 'Para grandes empresas',
+                    precio: '800$',
+                    caracteristicas: [
+                        "Autoadministrable",
+                        "Wordpress",
+                        "Seo"
+                    ]
+                }
+            ]
+        }
+    }
     render(){
         return(
             <section className="h-100 flex bg-tarifa pad-content jus-center">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                {this.state.tarifas.map((tarifa)=>{
+                    return(<Card nombre={tarifa.name} descripcion={tarifa.descripcion} precio={tarifa.precio}></Card>)
+                })}
             </section>
         )
     }
