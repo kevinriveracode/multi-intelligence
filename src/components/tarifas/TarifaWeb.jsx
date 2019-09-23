81159cc0a43dd76054d075683be908e3b1117989
Kevin <kevinrivera.a4b@gmail.com>
2019-09-23 16:58:58 +0200

    add -> Features prices

--- src/components/tarifas/TarifaWeb.jsx
+++ src/components/tarifas/TarifaWeb.jsx
@@ -3,20 +3,25 @@ import React , {Component} from 'react'
 class Card extends Component{
     render(){
         return(
-            <div className="w-25 bg-card">
+            <div className="w-25 bg-card mgw2 pad-full-1 radius shadow jus-center">
                 <div>
-                    <h3>Pack Basic</h3>
-                    <p>Para pymes que buscan crecer</p>
+                    <h3 className="tx-bold tx-center tx-blanco">Pack Basic</h3>
+                    <p className="tx-blanco tx-center">Para pymes que buscan crecer</p>
                 </div>
                 <div>
-                    <h2>$399</h2>
+                    <h2 className="super-title tx-center tx-bold">$399</h2>
                 </div>
                 <div>
                     <ul className="no-list">
-                        <li>Feature 1</li>
-                        <li>Feature 2</li>
-                        <li>Feature 3</li>
+                        <li className="tx-blanco">Feature 1</li>
+                        <li className="tx-blanco">Feature 2</li>
+                        <li className="tx-blanco">Feature 3</li>
                     </ul>
+                </div>
+                <div>
+                    <button className="btn-card radius w-80">
+                        Elegir plan
+                    </button>
                 </div>
             </div>
         )
@@ -25,7 +30,7 @@ class Card extends Component{
 class TarifaWeb extends Component{
     render(){
         return(
-            <section className="w-100  flex bg-tarifa pad-content ">
+            <section className="flex bg-tarifa pad-content jus-center">
                 <Card></Card>
                 <Card></Card>
                 <Card></Card>
